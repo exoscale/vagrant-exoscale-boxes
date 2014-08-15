9a3af5ae3c457b8fd7b00448b6e3b811cb2ba894
Loic Lambiel <lll@exoscale.ch>
2014-08-15 10:29:03 +0200

    refresh centos 6.5

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 #
 
 # Account access for Open Cloud to display usefull information
-apikey='VJtOMaCmypDsgv6LAUQ9CiedAm2L7xACSqSbERnPSQU0BfEbWqz7K1ywA6XnpyiP47L890d_JUwOk5EUh0qbAA'
-secretkey='ja2AcM1u0KlEGoepg6cZVc6NjkmsYDjIZEYKU3KUH1Fi_0BEx4jMW18nzp-uac7tkeoMm7C2w3EbR1Qbdl7qDQ'
+apikey='DDg7q4HDX1LMrQfxjPdhHef3x1ncs66ldaWaFDcp3y-zsGuZmJronMrSBoxxqAb0OlUOv4umvLC3lW8nx_OdBA'
+secretkey='daT1ketWCaDgC3KGQJSElG-LXMQKXAhTEpvRagltkWIkRZZOGRb0szaWTRdJoiiZIMkU_gGUNMtKgll7x7tGYA'
 path='/compute'
 host='api.exoscale.ch'
 port='443'

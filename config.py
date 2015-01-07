40af2296684370dabfecb1a76d33d28e43a99031
Loic Lambiel <lll@exoscale.ch>
2015-01-07 13:24:05 +0100

    image refresh

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 #
 
 # Account access for Open Cloud to display usefull information
-apikey='puNYFLz3yBTcQwrH8NvsEkQGYEcuW9ZLKVrDwCDOHuX8ls-1G-WDgtbH0nYOsAR2yrlVtal6eLhjMhQD3KKvDw'
-secretkey='tHXCU_6DqaXpD_Abjad7kqRE-a4eA9FlGmnKuVhPB1ektzQfvTp5UcFCD0k3wIR30RGv-uIzQD3b9_CP75g4qw'
+apikey='Kbu3sBLHhG67nhUo5VEdM9uS0Iw9803DiRzskRbjrZJMCT_SdEBIwI0YesoXvTyOMUTwqdKZKQDl_UosAlCUTg'
+secretkey='l3a3NnCMpWuO7SHrHUVqANDc2169XEeedo6-qWKhLY6zwCaSCIqjI0Mhc2n19A_N2qwx20oW6nKwkngbNnhh3g'
 path='/compute'
 host='api.exoscale.ch'
 port='443'
